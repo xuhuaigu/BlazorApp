0f0751d83353e21c3379c88911126fd2c428fc2c
xuhuaigu <167395025+xuhuaigu@users.noreply.github.com>
2024-07-26 23:24:30 +0800

    Add files via upload

---
+++ BlazorApp/TodoItem.cs
@@ -0,0 +1,36 @@
+﻿namespace BlazorApp
+{
+    public class TodoItem
+    {
+        public string? Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+
+    public class Test
+    {
+        public int rnd = new Random().Next(100, 200);
+        public List<int> number1 = new List<int>() { new Random().Next(100, 200),
+                                                    new Random().Next(100, 200),
+                                                    new Random().Next(100, 200),
+                                                    new Random().Next(100, 200),
+                                                    new Random().Next(100, 200),
+                                                    new Random().Next(100, 200),
+                                                    new Random().Next(100, 200),
+                                                    new Random().Next(100, 200),
+                                                    new Random().Next(100, 200),
+        };
+
+        public object[] DATA = new object[] 
+        {
+            new  { year = "1991", value = new Random().Next(100, 200)},
+            new  { year = "1992", value = new Random().Next(100, 200) },
+            new  { year = "1993", value = new Random().Next(100, 200) },
+            new  { year = "1994", value = new Random().Next(100, 200) },
+            new  { year = "1995", value = new Random().Next(100, 200) },
+            new  { year = "1996", value = new Random().Next(100, 200) },
+            new  { year = "1997", value = new Random().Next(100, 200) },
+            new  { year = "1998", value = new Random().Next(100, 200) },
+            new  { year = "1999", value = new Random().Next(100, 200) },
+        };
+    }
+}
